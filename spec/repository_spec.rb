3ff652863685ad7133716332c2ebb71ad4b82194
Vincent Oostindie <vincent@ulso.nl>
2016-06-25 16:45:43 +0200

    Fix repository tests: don't use class instance variables.

--- spec/repository_spec.rb
+++ spec/repository_spec.rb
@@ -17,8 +17,8 @@ describe Topicz::Repository do
 
   it 'should read a topic YAML file' do
     with_testdata do
-      @repository = Topicz::Repository.new('/topics')
-      topic = @repository.topics[0]
+      repository = Topicz::Repository.new('/topics')
+      topic = repository.topics[0]
       expect(topic.id).to eq 'topic_1'
       expect(topic.title).to eq 'Special topic'
       expect(topic.fullpath).to eq '/topics/topic1'
@@ -27,8 +27,8 @@ describe Topicz::Repository do
 
   it 'should find a topic based on the path' do
     with_testdata do
-      @repository = Topicz::Repository.new('/topics')
-      topics = @repository.find_all('topic1')
+      repository = Topicz::Repository.new('/topics')
+      topics = repository.find_all('topic1')
       expect(topics.length).to be 1
       expect(topics[0].id).to eq 'topic_1'
     end
@@ -36,8 +36,8 @@ describe Topicz::Repository do
 
   it 'should find a topic based on the title' do
     with_testdata do
-      @repository = Topicz::Repository.new('/topics')
-      topics = @repository.find_all('Special topic')
+      repository = Topicz::Repository.new('/topics')
+      topics = repository.find_all('Special topic')
       expect(topics.length).to be 1
       expect(topics[0].id).to eq 'topic_1'
     end
@@ -45,8 +45,8 @@ describe Topicz::Repository do
 
   it 'should find a topic based on an alias' do
     with_testdata do
-      @repository = Topicz::Repository.new('/topics')
-      topics = @repository.find_all('alias1')
+      repository = Topicz::Repository.new('/topics')
+      topics = repository.find_all('alias1')
       expect(topics.length).to be 1
       expect(topics[0].id).to eq 'topic_1'
     end
@@ -54,8 +54,8 @@ describe Topicz::Repository do
 
   it 'should find all topics with an empty search filter' do
     with_testdata do
-      @repository = Topicz::Repository.new('/topics')
-      topics = @repository.find_all
+      repository = Topicz::Repository.new('/topics')
+      topics = repository.find_all
       expect(topics.length).to be 3
     end
   end
